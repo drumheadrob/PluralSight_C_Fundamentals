2b039587af1db1805e0c932783a83f1048612fd8
Rob Allsop <drumheadrob@live.com>
2019-12-16 17:01:18 +0000

    Progress update

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,37 +9,8 @@ namespace GradeBook
         {
             var book = new Book("Rob's Grade Book");
             book.GradeAdded += OnGradeAdded;
-            
 
-            while(true) 
-            {
-                Console.WriteLine("Please enter grade, followed by enter key. Type 'q' to quit");
-                var input = Console.ReadLine();
-                if(input == "q")
-                {
-                    break;
-                }
-
-                try 
-                {
-                    var grade = double.Parse(input);
-                    book.AddGrade(grade);
-                }
-                catch(ArgumentException ex)
-                {   
-                    Console.WriteLine(ex.Message);
-                }
-                catch(FormatException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("**");
-                }
-            };
-
- 
+            EnterGrades(book);
 
             book.GetStatistics();
 
@@ -48,6 +19,37 @@ namespace GradeBook
             Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"Average: {stats.Average:N1}. Lowest grade: {stats.Low}. Highest grade {stats.High}");
             Console.WriteLine($"The letter grade is {stats.letter}");
+        }
+
+        private static void EnterGrades(Book book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter grade, followed by enter key. Type 'q' to quit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            };
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
